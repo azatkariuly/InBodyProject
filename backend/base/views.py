84f79cbcd65e5f9379baa3b775119516c3bc251c
Azat Azamat <azatkariuly@Azats-MacBook-Pro-2.local>
2024-04-17 14:41:32 +0900

    initial commit

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import requests
 
 # Create your views here.
 @api_view(['POST'])
 def handleRequest(request):
-    print('hello from world', request)
-    print('down', request.data.get('UserId'))
 
-    print('data', list(request.data.items()))
-    print('post', list(request.POST.items()))
+    url = 'https://apikr.lookinbody.com/inbody/GetFullInBodyDataByID'
 
-    return Response({"result": "request received"})
+    headers = {
+        'Content-Type': 'application/json',
+        'API-KEY': 'OilU7HR9nOzy7XQOvOYj7uyaBZf/lzlAysy+OIDtEF8=',
+        'Account': 'devcors07',
+    }
+
+    data = {
+        'UserID': request.data.get('UserId'),
+        'Datetimes': request.data.get('TestDatetimes'),
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    # Checking the response
+    if response.status_code == 200:
+        print("Request was successful!")
+        print("Response body:", response.text)
+        return Response({"result": "success"})
+    else:
+        print("Request failed with status code:", response.status_code)
+
+    return Response({"result": "fail"})
